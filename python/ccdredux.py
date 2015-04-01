f6574a08a6703663cf333b5cd10b6916248397b7
Chris Fassnacht <cdfassnacht@ucdavis.edu>
2015-04-01 04:15:11 -0700

    Moved 3 functions from suprimecam redux to ccdredux

--- python/ccdredux.py
+++ python/ccdredux.py
@@ -1149,6 +1149,353 @@ def make_wht_from_pixval(infile, maxgood, inwhtfile=None, outsuff='_wht',
    """ Clean up """
    del indat
 
+#------------------------------------------------------------------------------
+
+def make_wht_for_final(infiles, medfile, nsig, inwht_suff='.weight.fits',
+                       outwht_suff='_wht.fits', flag_posonly=False, 
+                       medwhtfile='default'):
+   """
+   Creates a weight file for each input image to a final swarp call.
+   This weight file will assign zero weight to pixels that differ by
+   more than nsig*rms from the median-stacked image that was created
+   in an earlier step.
+   Therefore, this function is a lot like the "blot" step in multidrizzle.
+
+   Inputs:
+      infiles      - list of individual input fits files that will be compared
+                      to the median stack.  These will probably be called 
+                      *resamp.fits
+      medfile      - median-stacked fits file
+      nsig         - minimum sigma difference between an individual input image
+                     and the median stacked image that will cause a pixel to be
+                     flagged.
+      flag_posonly - Sets flagging behavior.  If flag_posonly=True then only
+                     flag pixels where the value in the individual image is
+                     more than nsig*rms _larger_ than the value in the
+                     median-stacked image.
+                     If False, then flag pixels for which the absolute
+                     value of the difference is more than nsig*rms
+                     Explanation: Set to True to flag, e.g., cosmic rays in
+                     the individual image and to avoid flagging
+      medwhtfile   - Name of the weight file associated with medfile.  The
+                     default value ('default') will take the name of the
+                     median-stacked image (medfile) and replace '.fits' with
+                     '_wht.fits'
+   """
+
+   import imfuncs as imf
+   from math import sqrt
+
+   """ Make sure that the input is either a list or a single file """
+
+   if type(infiles) is str:
+      print ""
+      print "Single input file"
+      tmplist = [infiles,]
+   elif type(infiles) is list:
+      print ""
+      print "Input file list with %d members" % (len(infiles))
+      tmplist = infiles
+   else:
+      print ""
+      print "Warning.  Input frames need to be either a list of files "
+      print " (python type==list) or a single input file name."
+      print ""
+      return
+
+   """ First loop through the input files to get information """
+   gain = n.zeros(len(tmplist))
+   bkgd = n.zeros(len(tmplist))
+   fscal = n.zeros(len(tmplist))
+   x1 = n.zeros(len(tmplist),dtype=int)
+   y1 = n.zeros(len(tmplist),dtype=int)
+   rmssky = n.zeros(len(tmplist))
+
+   print ''
+   print ' Input file                  gain  <bkgd>   fscal  rms_sky rms_scal'
+   print '---------------------------- ---- -------- ------- ------- --------'
+   for i in range(len(tmplist)):
+
+      """ Load the header information """
+      f = tmplist[i]
+      try:
+         hdr = pf.getheader(f)
+      except:
+         print ""
+         print "ERROR: Could not open %s" % f
+         print ""
+         return
+
+      origfile = f.replace('resamp.fits','fits')
+      try:
+         orighdr = pf.getheader(origfile)
+      except:
+         print ""
+         print "ERROR: Could not open %s" % origfile
+         print ""
+         return
+
+      """ Set the relevant values """
+      x1[i]     = int(hdr['comin1'] - 1)
+      y1[i]     = int(hdr['comin2'] - 1)
+      bkgd[i]   = hdr['backmean']
+      fscal[i]  = hdr['flxscale']
+      gain[i]   = orighdr['gain']
+      rmssky[i] = sqrt(bkgd[i] / gain[i])
+
+      """ Print out the relevant information and clean up """
+      print '%-28s %4.2f %8.2f %7.5f %7.3f %8.3f' \
+          % (f[:-5],gain[i],bkgd[i],fscal[i],rmssky[i],rmssky[i]*fscal[i])
+      del hdr,orighdr
+
+   gainmean = gain.mean()
+   print '-------------------------------------------------------------------'
+   print ' %3d input files             gain  <bkgd>   fscal  rms_sky rms_scal'\
+       % (len(tmplist))
+   print '---------------------------- ---- -------- ------- ------- --------'
+   print '    Mean values              %4.2f %8.2f %7.5f %7.3f %8.3f' % \
+       (gainmean,bkgd.mean(),fscal.mean(),rmssky.mean(), \
+           rmssky.mean()*fscal.mean())
+
+   """ Set up the weight file associated with the median-stacked image """
+   if medwhtfile == 'default':
+      medwhtfile = medfile.replace('.fits','_wht.fits')
+
+   """ Open up the median file and its associated weight image"""
+   medall = pf.getdata(medfile)
+   medwhtall = pf.getdata(medwhtfile)
+
+   """ Loop through the input files """
+   epsil = 1.e-20
+   count = 1
+   print ''
+   print 'Updating weight files, using diff > %d sigma' % nsig
+   print '--------------------------------------------------------------------'
+   for i in range(len(tmplist)):
+      """ Set up file names """
+      f = tmplist[i]
+      inwhtfile = f.replace('.fits',inwht_suff)
+      outwhtfile = f.replace('.fits',outwht_suff)
+
+      """ Load input resamp.fits file """
+      print '%s -- File %d of %d' % (f[:-5],count,len(tmplist))
+      print '-----------------------------------------'
+      print 'Loading individual exposure: %s' % f
+      try:
+         indat,hdr = pf.getdata(f,header=True)
+      except:
+         print ""
+         print "ERROR: Could not open %s" % f
+         print ""
+         return
+
+      """ Load the associated weight file data """
+      try:
+         inwht,whthdr = pf.getdata(inwhtfile,header=True)
+      except:
+         print ""
+         print "ERROR: Could not open %s" % inwhtfile
+         print ""
+         return
+
+
+      """ Set up the relevant region to be examined """
+      x2 = int(x1[i] + indat.shape[1])
+      y2 = int(y1[i] + indat.shape[0])
+
+      """ 
+      Make the cutout of the median-stack image and then take the 
+      difference between this file and the scaled input individual file
+      """
+        #meddat = (pf.getdata(medfile))[y1[i]:y2,x1[i]:x2]
+        #medwht = (pf.getdata(medwhtfile))[y1[i]:y2,x1[i]:x2]
+        #meddat = medhdu[0].section[y1[i]:y2,x1[i]:x2]
+        #medwht = medwhthdu[0].section[y1[i]:y2,x1[i]:x2]
+      print 'Selecting data from full stacked median file'
+      meddat = medall[y1[i]:y2,x1[i]:x2]
+      medwht = medwhtall[y1[i]:y2,x1[i]:x2]
+      print 'Calculating difference image'
+      diff = n.zeros(indat.shape)
+      whtmask = inwht>0
+      diff[whtmask] = indat[whtmask] * fscal[i] - meddat[whtmask]
+      del whtmask
+
+      """ 
+      Get an estimate of the RMS noise in the data.  Since we are creating
+      a difference, indat - meddat, the final rms will be
+          sigma_diff^2 = sigma_ind^2 + sigma_med^2
+      The input variances will come from:
+
+          sigma_ind^2 = (inddat+bkgd) / gain
+            - inddat is a background-subtracted image with units of ADU.
+              Therefore, add back in the background and then divide by
+              the gain to get the variance in each pixel.
+              This is because sigma_ADU^2 = ADU/gain, assuming that
+              N_e = gain * ADU is a Poisson process.
+
+          sigma_med^2 = (1. / medwht) + [for SNR>1: meddat/gain] 
+            - Swarp produces an inverse-variance weight map
+              NOTE: However, this does NOT include Poisson noise from the
+              objects in the image.  This is because computes this Poisson
+              noise when it does its object detections, and therefore 
+              expects any input weight file to not include the Poisson noise.
+
+      """
+      print 'Masking and then calculating variance from stacked median'
+      medvar = n.zeros(indat.shape)
+      medmask = (inwht>0) & (n.absolute(medwht>epsil))
+      medvar[medmask] = 1. / medwht[medmask]
+      del medmask
+      print 'Masking and then adding Poisson noise from stacked median'
+      medmask = meddat > n.sqrt(medvar)
+      medvar[medmask] += meddat[medmask] / gainmean
+      del medmask
+      #del medwht,meddat
+      print 'Masking and then calculating variance in individual image'
+      indvar = n.zeros(indat.shape)
+      mask = (inwht>0) & ((indat + bkgd[i]) > 0.)
+      indvar[mask] = fscal[i]**2 * (indat[mask]+bkgd[i])/gain[i]
+      print 'Calculating combined rms'
+      rms = n.sqrt(medvar + indvar)
+      del medvar,indvar,mask,indat
+
+      """ 
+      Flag pixels that deviate by more than nsig sigma from the 
+      median-stacked image
+      """
+      print 'Flagging pixels that differ by more than %d sigma from median'\
+          % nsig
+      if flag_posonly:
+         blotmask = diff > nsig*rms
+      else:
+         blotmask = n.absolute(diff) > nsig*rms
+      print '%-38s %9d' \
+          % (f[:-5],blotmask.sum())
+
+      """ Debugging step(s) """
+      #foodat = n.ones(diff.shape)
+      #foodat[blotmask] = 0
+      #fooname = f.replace('.fits','_foo.fits')
+      #pf.PrimaryHDU(foodat).writeto(fooname,clobber=True)
+      #del foodat
+
+      #rmsname = f.replace('.fits','_rms.fits')
+      #pf.PrimaryHDU(rms,hdr).writeto(rmsname,clobber=True)
+
+      #snr = n.zeros(diff.shape)
+      #rmsmask = rms>0
+      #snr[rmsmask] = diff[rmsmask] / rms[rmsmask]
+      #snrname = f.replace('.fits','_snr.fits')
+      #pf.PrimaryHDU(snr,hdr).writeto(snrname,clobber=True)
+      #del snr
+
+      """ Write out a new weight file with the newly-flagged pixels """
+      inwht[blotmask] = 0
+      print '%s -> %s' % (inwhtfile,outwhtfile)
+      pf.PrimaryHDU(inwht,whthdr).writeto(outwhtfile,clobber=True)
+      count += 1
+      print ''
+
+      """ Close the files for this loop """
+      del diff,blotmask,rms,inwht
+
+   """ Clean up """
+   #medhdu.close()
+   #medwhthdu.close()
+   del medall,medwhtall
+
+#---------------------------------------------------------------------------
+
+def add_exptime(inlist, exptime, hext=0, verbose=True):
+   """
+   Given a list of fits files, adds to each one an EXPTIME header keyword
+   with a value given by the passed exptime parameter
+
+   Inputs:
+     inlist  - list of fits files to which the keyword will be added
+     exptime - the value of the exposure time to add to each file
+     hext    - HDU to modify (default = 0)
+     verbose - Set to True (the default) for some status
+   """
+
+   for i in inlist:
+      hdu = pf.open(i,mode='update')
+      hdr = hdu[hext].header
+      hdr.update('exptime',exptime)
+      hdu.flush()
+      if verbose:
+         print 'Updated %s with EXPTIME=%.2f' % (i,exptime)
+
+#---------------------------------------------------------------------------
+
+def make_texp_map(infiles, texp, whtext='wht', outext='texp'):
+   """
+   Uses the weight files in the input list to create individual exposure
+   time maps for each resampled image.
+   NOTE: The input weight files need to be the resampled and then 
+   modified ones that are created with the make_wht_for_final function
+
+   Inputs:
+      infiles  - list of input files, perhaps created with a glob.glob call
+      texp     - exposure time for the original image
+      whtext   - extension on input files, which should be the weight
+                 files.  The default, 'wht' means that the files in infiles
+                 are expected to be called [root]_wht.fits
+      outext   - extension used for output exposure-time file name. In other
+                   words, for an input file of [root]_[whtext].fits the output 
+                   file name will be [root]_[outext].fits
+                 Default: 'texp'
+
+   Outputs:
+      Each input file called [root]_[whtext].fits will produce an output 
+       exposure time file called [root]_[outext].fits
+   """
+
+   """ Make sure that the input is either a list or a single file """
+
+   if type(infiles) is str:
+      print ""
+      print "Single input file"
+      tmplist = [infiles,]
+   elif type(infiles) is list:
+      print ""
+      ncount = len(infiles)
+      print "Input file list with %d members" % ncount
+      tmplist = infiles
+   else:
+      print ""
+      print "Warning.  Input frames need to be either a list of files "
+      print " (python type==list) or a single input file name."
+      print ""
+      return
+
+   """ Run through the list """
+
+   print ''
+   print 'Making individual exposure-time maps'
+   print '------------------------------------'
+   count = 1
+   for f in tmplist:
+      """ Get input file"""
+      print 'File %d of %d' % (count,ncount)
+      print 'Input file:  %s' % f
+      data,hdr = pf.getdata(f,header=True)
+
+      """ Create the exposure time data as integer """
+      data[data>0] = texp
+      data = data.astype(int)
+      hdr['object'] = 'Exposure time map'
+
+      """ Write the output file """
+      outfile = f.replace('%s.fits' % whtext,'%s.fits' % outext)
+      pf.PrimaryHDU(data,hdr).writeto(outfile)
+      print 'Output file: %s' % outfile
+      print ''
+
+      """ Clean up """
+      del data,hdr
+      count += 1
+
 #-----------------------------------------------------------------------
 
 def split_imext(infits, next, outroot=None):
