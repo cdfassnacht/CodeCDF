8ac7fc6eb6ff7595b0a6e981360fd803abf0b017
Chris Fassnacht <cdfassnacht@ucdavis.edu>
2016-04-29 12:10:31 -0700

    Started a GLens class, but have done nothing with it yet

---
+++ python/lensfuncs.py
@@ -0,0 +1,11 @@
+"""
+For now, nothing.
+
+A future step 1: a class GLens that will hold relevant information about a
+gravitational lens system and calculate some relevant parameters
+"""
+
+import numpy as np
+from astropy import constants
+
+class GLens:
