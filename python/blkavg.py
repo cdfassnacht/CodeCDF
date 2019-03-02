ed6e724184e200e113fd24e55da5bbc8442e07c1
Chris Fassnacht <cdfassnacht@ucdavis.edu>
2019-03-02 12:33:32 -0800

    Updated blkavg.py to use the new blkavg method in the Image class

--- python/blkavg.py
+++ python/blkavg.py
@@ -18,6 +18,7 @@ Usage: python blkavg.py [input_fits] [output_fits] [blkavg_factor]
 """
 
 import sys
+from specim import imfuncs as imf
 import numpy as n
 try:
     from astropy.io import fits as pf
@@ -30,52 +31,16 @@ except:
         print ''
         exit()
 
-#---------------------------------------------------------------------------
-
-def resamp(a,factor,add=False):
-    """
-    Code to do the block averaging, taken from Matt Auger's indexTricks library
-
-    Inputs:
-       a      - input data array
-       factor - block averaging / summing factor
-       add    - set to True if desired output is sum rather than average
-                default = False
-
-    Output:
-       o      - block-averaged or block-summed array
-    """
-    arr = a.copy()
-    """ 
-    Cut off rows and columns to get an integer multiple of the factor
-    in each dimension
-    """
-    dx = arr.shape[1] % factor
-    dy = arr.shape[0] % factor
-    if dx>0:
-        arr = arr[:,:-dx]
-    if dy>0:
-        arr = arr[:-dy,:]
-    x = arr.shape[1]/factor
-    y = arr.shape[0]/factor
-    o = n.zeros((y,x))
-    for i in range(factor):
-        for j in range(factor):
-            o += arr[i::factor,j::factor]
-    if add==True:
-        return o
-    return o/factor**2
-
 #-----------------------------------------------------------------------
 
 """ Main program """
 
 """ Check command-line format """
 if len(sys.argv)<4:
-    print ''
-    print 'Usage: python blkavg.py [input_fits] [output_fits] [blkavg_factor]'
-    print '  Example:  python blkavg.py big_file.fits ave_file.fits 3'
-    print ''
+    print('')
+    print('Usage: python blkavg.py [input_fits] [output_fits] [blkavg_factor]')
+    print('  Example:  python blkavg.py big_file.fits ave_file.fits 3')
+    print('')
     exit()
 
 """
@@ -86,7 +51,7 @@ infits  = sys.argv[1]
 outfits = sys.argv[2]
 try:
     blkfact = int(sys.argv[3])
-except:
+except ValueError:
     print ''
     print 'ERROR. Third command-line input cannot be converted to an integer'
     print ''
@@ -97,15 +62,10 @@ Do the averaging.
 NOTE: For now assume data are in HDU 0
 """
 
-try:
-    (indat,hdr) = pf.getdata(infits,header=True)
-except:
-    print ''
-    print 'ERROR.  Could not read data from input file %s' % infits
-    print ''
-    exit()
-outdat = resamp(indat,blkfact)
+indat = imf.Image(infits)
+indat.blkavg(3, outfile=outfits)
 
-""" Save the output file, clean up, and exit """
-pf.PrimaryHDU(outdat,header=hdr).writeto(outfits)
-del indat,outdat,hdr
+
+""" Clean up, and exit """
+indat.close()
+del indat
